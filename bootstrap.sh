59677bcbafc68c9bd07d1522c37f7ddce79364a7
Bekker Stacks <50801771+bekkerstacks@users.noreply.github.com>
2019-05-20 14:40:07 +0200

    v1 of bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,28 @@
+#!/bin/sh
+set -ex
 
+MONGODB_USERNAME="dbadmin"
+MONGODB_PASSWORD="dbpasswd"
+
+count=0
+echo "booting mongodb server"
+mkdir -p /data/db
+mongod --syslog --storageEngine wiredTiger --smallfiles --dbpath /data/db --bind_ip_all --port 27017 &
+while [ $count -le 10 ]
+  do
+    sleep 1
+    count=$((count + 1))
+  done
+
+echo "creating admin user"
+mongo <<EOF
+use admin
+db.createUser({user: "${MONGODB_USERNAME}", pwd: "${MONGODB_PASSWORD}", roles:[{role: "root", db: "admin"}]})
+EOF
+
+sleep 2
+echo "tear down"
+kill $!
+
+echo "starting server with auth"
+mongod --syslog --storageEngine wiredTiger --smallfiles --dbpath /data/db --bind_ip_all --port 27017 --auth
